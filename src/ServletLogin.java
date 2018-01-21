99b1d091ec4111e849539b659868a0e063056737
yaroslav <gavryshko.sl@gmail.com>
2018-01-21 18:47:59 +0200

    Login servlet[Init commit]

---
+++ src/ServletLogin.java
@@ -0,0 +1,53 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by yaroslav on 21.01.18.
+ */
+@WebServlet(name = "LoginServlet")
+public class ServletLogin extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        request.getRequestDispatcher("Login.html").include(request, response);
+
+        String name=request.getParameter("name");
+        String password=request.getParameter("password");
+
+        if(password.equals("admin")) {
+            out.print("Welcome, " + name);
+        }else {
+            out.print("Uppps..sorry no such user");
+        }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        try (PrintWriter writer = response.getWriter()) {
+
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>ServletLogin.java:doGet(): Servlet code!</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>This is a simple java servlet.</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,15 @@
+<%-- Created by IntelliJ IDEA. --%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<body>
+<form method="post" action="/ServletLogin">
+  Email ID:<input type="text" name="name" /><br/>
+  Password:<input type="text" name="password" /><br/>
+  <input type="submit" value="login" />
+</form>
+</head>
+
+</body>
+</html>
